d8c6c772b446d6fc7bfe6bb1d9ef05a88ca57ed4
Pablo-Ali <pablosharifali@gmail.com>
2024-10-10 15:22:38 +0000

    fix: agregados los números de las particiones en el montaje persistente, lo que llevaba a un error

--- RTA_SCRIPT_Examen_20241009/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241009/Punto_B.sh
@@ -52,16 +52,16 @@ sudo mkfs.ext4 ${DISCO}12
 sudo mkfs.ext4 ${DISCO}13
 sudo mkfs.ext4 ${DISCO}14
 
-echo "${DISCO} /Examenes-UTN/alumno_1/parcial_1  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/alumno_1/parcial_2  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/alumno_1/parcial_3  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/alumno_2/parcial_1  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/alumno_2/parcial_2  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/alumno_2/parcial_3  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/alumno_3/parcial_1  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/alumno_3/parcial_2  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/alumno_3/parcial_3  ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "${DISCO} /Examenes-UTN/profesores  	 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}5 /Examenes-UTN/alumno_1/parcial_1  ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}6 /Examenes-UTN/alumno_1/parcial_2  ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}7 /Examenes-UTN/alumno_1/parcial_3  ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}8 /Examenes-UTN/alumno_2/parcial_1  ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}9 /Examenes-UTN/alumno_2/parcial_2  ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}10 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}11 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}12 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}13 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "${DISCO}14 /Examenes-UTN/profesores  	  ext4 defaults 0 0" | sudo tee -a /etc/fstab
 
 sudo mount -a
 
