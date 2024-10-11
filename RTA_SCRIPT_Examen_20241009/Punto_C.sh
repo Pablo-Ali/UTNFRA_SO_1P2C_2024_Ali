f6706b6e175ff6e9f9f70b21c7f0b7281e7062e4
Pablo-Ali <pablosharifali@gmail.com>
2024-10-11 20:28:32 +0000

    feat: agregados los comandos para crear los archivos txt y sus cambios de permisos

--- RTA_SCRIPT_Examen_20241009/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241009/Punto_C.sh
@@ -15,7 +15,18 @@ sudo chown -R p1c2_2024_A2:p1c2_2024_gAlumno /Examenes-UTN/alumno_2
 sudo chown -R p1c2_2024_A3:p1c2_2024_gAlumno /Examenes-UTN/alumno_3
 sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
 
-sudo chmod 750 /Examenes-UTN/alumno_1 
-sudo chmod 760 /Examenes-UTN/alumno_2
-sudo chmod 700 /Examenes-UTN/alumno_3
-sudo chmod 770 /Examenes-UTN/profesores
+sudo chmod -R 750 /Examenes-UTN/alumno_1 
+sudo chmod -R 760 /Examenes-UTN/alumno_2
+sudo chmod -R 700 /Examenes-UTN/alumno_3
+sudo chmod -R 770 /Examenes-UTN/profesores
+
+sudo su -c "whoami > /Examenes-UTN/alumno_1/validar.txt" p1c2_2024_A1
+sudo su -c "whoami > /Examenes-UTN/alumno_2/validar.txt" p1c2_2024_A2
+sudo su -c "whoami > /Examenes-UTN/alumno_3/validar.txt" p1c2_2024_A3
+sudo su -c "whoami > /Examenes-UTN/profesores/validar.txt" p1c2_2024_P1
+
+sudo chmod 750 /Examenes-UTN/alumno_1/validar.txt
+sudo chmod 760 /Examenes-UTN/alumno_2/validar.txt
+sudo chmod 700 /Examenes-UTN/alumno_3/validar.txt
+sudo chmod 770 /Examenes-UTN/profesores/validar.txt
+
